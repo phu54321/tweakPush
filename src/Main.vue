723a0239e68db1cd910bbf8f8d69ee2c6b5344ea
박현우 <phu54321@naver.com>
2019-02-10 13:58:45 +0900

    basic gui

--- src/Main.vue
+++ src/Main.vue
@@ -1,10 +1,14 @@
 <template lang='pug'>
 nb-container
   nb-header
+    nb-left
+      nb-button(:transparent='true', iconLeft, @press='showAbout()')
+        nb-icon(name='information-circle-outline')
     nb-body
       nb-title TweakPush
+    nb-right
   nb-content(v-if='debFiles.length > 0')
-    nb-list-item(v-for='file in debFiles', @press='onDebClick(file)', :selected='isFileSelected(file)')
+    nb-list-item(v-for='file in debFiles', @press='onDebClick(file)')
       nb-text(:style="{color: colorHash(file), paddingRight: 20}") ■
       nb-text {{file}}
   nb-content.container-no-deb(:padder='true', v-else, :contentContainerStyle="{ justifyContent: 'center', alignItems: 'center', flex: 1 }")
@@ -12,11 +16,11 @@ nb-container
 
   nb-footer
     nb-footer-tab
-      nb-button(@press='install', :variant='selectedFiles.length === 0 ? "secondary" : "primary"')
-        nb-icon(name='download')
-        nb-text(primary) Install
-      nb-button(@press='respring')
+      nb-button(@press='refreshList')
         nb-icon(name='refresh')
+        nb-text(warning) Reload .deb files
+      nb-button(@press='respring')
+        nb-icon(name='power')
         nb-text(warning) Respring
 </template>
 
@@ -30,25 +34,25 @@ const colorHash = new ColorHash()
 export default {
   data () {
     return {
-      debFiles: [],
-      selectedFiles: []
+      debFiles: []
     }
   },
   async created () {
-    this.debFiles = (await Expo.FileSystem.readDirectoryAsync('file:///var/mobile/Media/general_storage'))
-      .filter(x => x.endsWith('.deb'))
+    this.refreshList()
   },
   methods: {
     colorHash (s) {
       return colorHash.hex(s)
     },
     onDebClick (file) {
-      const index = this.selectedFiles.indexOf(file)
-      if (index === -1) this.selectedFiles.push(file)
-      else this.selectedFiles.splice(index, 1)
+      alert('Deb clicked: ' + file)
     },
-    isFileSelected (file) {
-      return this.selectedFiles.indexOf(file) !== -1
+    showAbout () {
+      alert('TweakPush v1 by trgk')
+    },
+    async refreshList () {
+      this.debFiles = (await Expo.FileSystem.readDirectoryAsync('file:///var/mobile/Media/general_storage'))
+      // .filter(x => x.endsWith('.deb'))
     }
   }
 }
